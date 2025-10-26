dfe683e6d63f1f5cc0d570653c1fb6ea473e3624
Jonny McNamee <jonathanmc.jm94@gmail.com>
2025-10-26 13:57:38 -0700

    Adding Fixes model and type

---
+++ server/models/fixes.ts
@@ -0,0 +1,65 @@
+import { Model, Column, DataType, Table, PrimaryKey, HasMany } from "sequelize-typescript";
+import type { Fix } from '../types/Fix.ts'
+
+@Table({
+    tableName: "fixes",
+    timestamps: true,
+})
+export class FixesModel extends Model<FixesModel> implements Fix {
+        @PrimaryKey
+        @Column({
+                type: DataType.UUID,
+                defaultValue: DataType.UUIDV4
+        })
+        id!: number;
+
+        @Column({
+                type: DataType.UUID,
+                allowNull: false
+        })
+        product_id!: number;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        title!: string;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        description!: string;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        steps!: string;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        source_type!: string;
+
+        @Column({
+                type: DataType.BOOLEAN,
+                allowNull: false
+        })
+        verified!: boolean;
+
+        @Column({
+                type: DataType.DATE,
+                defaultValue: DataType.NOW
+        })
+        created_at!: Date;
+
+        @Column({
+                type: DataType.DATE,
+                defaultValue: DataType.NOW
+        })
+        updated_at!: Date;
+
+}
+
---
+++ server/types/Fix.ts
@@ -0,0 +1,11 @@
+export type Fix = {
+    id: number;
+    product_id: number;
+    title: string;
+    description: string;
+    steps: string;
+    source_type: string;
+    verified: boolean;
+    created_at: Date;
+    updated_at: Date;
+}
