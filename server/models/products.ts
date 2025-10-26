e849bc879289324b4a82907377dce0c23b902ae1
Jonny McNamee <jonathanmc.jm94@gmail.com>
2025-10-26 13:47:04 -0700

    Setting up Products model and type

---
+++ server/models/products.ts
@@ -0,0 +1,53 @@
+import { Model, Column, DataType, Table, PrimaryKey, HasMany } from "sequelize-typescript";
+import type { Products } from '../types/Products.ts'
+
+@Table({
+    tableName: "products",
+    timestamps: true,
+})
+export class ProductsModel extends Model<ProductsModel> implements Products {
+        @PrimaryKey
+        @Column({
+                type: DataType.UUID,
+                defaultValue: DataType.UUIDV4
+        })
+        id!: number;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        name!: string;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        brand!: string;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        model_number!: string;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        category!: string;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        image_url!: string;
+
+        @Column({
+                type: DataType.DATE,
+                defaultValue: DataType.NOW
+        })
+        created_at!: Date;
+
+}
+
