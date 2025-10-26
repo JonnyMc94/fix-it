35e27c7910d4c6177149d0299d3c472bedc84f6f
Jonny McNamee <jonathanmc.jm94@gmail.com>
2025-10-26 14:08:45 -0700

    Adding updated manual model

---
+++ server/models/manuals.ts
@@ -0,0 +1,53 @@
+import { Model, Column, DataType, Table, PrimaryKey, HasMany } from "sequelize-typescript";
+import type { Manual } from '../types/Manual.ts'
+
+@Table({
+    tableName: "manuals",
+    timestamps: true,
+})
+export class ManualModel extends Model<ManualModel> implements Manual {
+        @PrimaryKey
+        @Column({
+                type: DataType.UUID,
+                defaultValue: DataType.UUIDV4
+        })
+        id!: number;
+
+        @Column({
+                type: DataType.UUID,
+                allowNull: false
+        })
+        product_id!: number;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        title!: string;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        file_url!: string;
+
+        @Column({
+                type: DataType.STRING,
+                allowNull: false
+        })
+        source_url!: string;
+
+        @Column({
+                type: DataType.DATE,
+                defaultValue: DataType.NOW
+        })
+        created_at!: Date;
+
+        @Column({
+                type: DataType.DATE,
+                defaultValue: DataType.NOW
+        })
+        updated_at!: Date;
+
+}
+
