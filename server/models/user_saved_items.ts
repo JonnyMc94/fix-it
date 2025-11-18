256886b5b4dd91853f1707b5b3de9d815f598dea
Jonny McNamee <jonathanmc.jm94@gmail.com>
2025-11-17 20:13:27 -0800

    Adding user saved items model and type

---
+++ server/models/user_saved_items.ts
@@ -0,0 +1,45 @@
+import { Model, Column, DataType, Table, PrimaryKey, HasMany } from "sequelize-typescript";
+import type { User_Saved_Item } from '../types/UserSavedItem.ts'
+
+@Table({
+    tableName: "user_saved_items",
+    timestamps: true,
+})
+export class UserSavedItemsModel extends Model<UserSavedItemsModel> implements User_Saved_Item {
+        @PrimaryKey
+        @Column({
+                type: DataType.UUID,
+                defaultValue: DataType.UUIDV4
+        })
+        id!: number;
+
+        @Column({
+                type: DataType.UUID,
+                allowNull: false
+        })
+        user_id!: number;
+
+        @Column({
+                type: DataType.UUID,
+        })
+        manual_id!: number;
+
+        @Column({
+                type: DataType.UUID,
+        })
+        fix_id!: number;
+
+        @Column({
+                type: DataType.DATE,
+                defaultValue: DataType.NOW
+        })
+        created_at!: Date;
+
+        @Column({
+                type: DataType.DATE,
+                defaultValue: DataType.NOW
+        })
+        updated_at!: Date;
+
+}
+
---
+++ server/types/UserSavedItem.ts
@@ -0,0 +1,8 @@
+export type User_Saved_Item = {
+    id: number;
+    user_id: number
+    manual_id?: number;
+    fix_id?: number;
+    created_at: Date;
+    updated_at: Date
+}
